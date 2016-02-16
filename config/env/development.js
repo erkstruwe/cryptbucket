5bb28caef39aaa08928fec263e869540866dcf2b
erkstruwe <estruwe@estruwe.de>
2016-02-16 20:20:33 +0100

    heroku settings

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
 	return {
-		port: 1336,
+		port: process.env.PORT,
 		baseUrl: '//localhost:1336',
 		baseUrlStatic: '//static.localhost:1336'
 	};
---
+++ config/env/production.js
@@ -0,0 +1,7 @@
+module.exports = function (app) {
+	return {
+		port: process.env.PORT,
+		baseUrl: '//cryptbucket.herokuapp.com',
+		baseUrlStatic: '//cryptbucket.herokuapp.com'
+	};
+};
