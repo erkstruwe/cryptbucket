767742639e105b8b9643e06e1a5e2df0e669e4f9
erkstruwe <estruwe@estruwe.de>
2016-02-18 18:33:42 +0100

    jquery, bootstrap js preparation

--- src/js/components/app/index.js
+++ src/js/components/app/index.js
@@ -1,8 +1,12 @@
+window.jQuery = require('jquery');
+//require('bootstrap/dist/js/umd/alert.js');
 var angular = require('angular');
 require('ng-file-upload');
+//require('angular-ng-bootstrap'); // (https://github.com/ng-bootstrap/core)
 
 var app = angular.module('cryptbucket', []);
 
 app.run(function () {
   console.log('Hello world');
 });
+
